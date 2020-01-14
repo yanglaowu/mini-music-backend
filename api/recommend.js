9acc2e3706a8fa8f1d92edbd26cf66323b2dbf87
杨杰 <>
2020-01-14 10:01:25 +0800

    update recommand api

--- api/recommend.js
+++ api/recommend.js
@@ -2,18 +2,73 @@ import {commonParams} from './config'
 import axios from 'axios'
 
 export function getRecommend () {
-  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg';
+  const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg';
+  const jumpPrefix = 'https://y.qq.com/n/yqq/album/';
 
   const data = Object.assign({}, commonParams, {
-    platform: 'h5',
-    uin: 0,
-    needNewCode: 1
+    platform: 'yqq.json',
+    hostUin: 0,
+    needNewCode: 0,
+    inCharset: 'utf8',
+    format: 'json',
+    '-': 'recom' + (Math.random() + '').replace('0.', ''),
+    data: {
+      'comm': { 'ct': 24 },
+      'category': { 'method': 'get_hot_category', 'param': { 'qq': '' }, 'module': 'music.web_category_svr' },
+      'recomPlaylist': {
+        'method': 'get_hot_recommend',
+        'param': { 'async': 1, 'cmd': 2 },
+        'module': 'playlist.HotRecommendServer'
+      },
+      'playlist': {
+        'method': 'get_playlist_by_category',
+        'param': { 'id': 8, 'curPage': 1, 'size': 40, 'order': 5, 'titleid': 8 },
+        'module': 'playlist.PlayListPlazaServer'
+      },
+      'new_song': { 'module': 'newsong.NewSongServer', 'method': 'get_new_song_info', 'param': { 'type': 5 } },
+      'new_album': {
+        'module': 'newalbum.NewAlbumServer',
+        'method': 'get_new_album_info',
+        'param': { 'area': 1, 'sin': 0, 'num': 10 }
+      },
+      'new_album_tag': { 'module': 'newalbum.NewAlbumServer', 'method': 'get_new_album_area', 'param': {} },
+      'toplist': { 'module': 'musicToplist.ToplistInfoServer', 'method': 'GetAll', 'param': {} },
+      'focus': { 'module': 'QQMusic.MusichallServer', 'method': 'GetFocus', 'param': {} }
+    }
   });
   return axios.get(url, {
+    headers: {
+      referer: 'https://u.y.qq.com/',
+      host: 'u.y.qq.com'
+    },
     params: data
   }).then(response => {
-    return response.data
-  } )
+    response = response.data
+    if (response.code === 0) {
+      const slider = []
+      const content = response.focus.data && response.focus.data.content
+      if (content) {
+        for (let i = 0; i < content.length; i++) {
+          const item = content[i]
+          const sliderItem = {}
+          sliderItem.id = item.id
+          sliderItem.linkUrl = jumpPrefix + item.jump_info.url + '.html'
+          sliderItem.picUrl = item.pic_info.url
+          slider.push(sliderItem)
+        }
+      }
+      return {
+        code: 0,
+        data: {
+          slider
+        }
+      }
+    } else {
+      return response
+    }
+  }).catch((e) => {
+    console.log(e)
+  })
 }
 
 export function getDiscList() {
